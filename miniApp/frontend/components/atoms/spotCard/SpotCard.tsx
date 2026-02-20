f29cfe49c5fcb797e5a481c1ae3a9509c6b1c577
ohgushi0329 <k21033ko@apps.kct.ac.jp>
2026-02-20 16:36:05 +0900

    fix: cssをmodule.cssに変更、imgタグをImageタグへ変更、マジックナンバーを修正しました

--- miniApp/frontend/components/atoms/spotCard/SpotCard.tsx
+++ miniApp/frontend/components/atoms/spotCard/SpotCard.tsx
@@ -1,9 +1,11 @@
 import { Button, IconButton, ThemeProvider, createTheme } from '@mui/material';
 import { X, Sun, Moon, User, Baby } from 'lucide-react';
-import './SpotCard.css';
+import styles from './SpotCard.module.css';
+import Image from "next/image"
 
 // 将来的にtypesディレクトリに移動
 type SpotKinds = "shop" | "spot";
+const MAX_TAGS = 4;
 
 // メインカラー
 // 将来的には別の定数フォルダに移動、もしくはpage.tsxでプロバイダで包む
@@ -72,7 +74,7 @@ export const SpotCard = ({
 
     const config = iconConfig[spotKind];
     // タグを最大4つに
-    const limitedTags = spotTags.slice(0, 4);
+    const limitedTags = spotTags.slice(0, MAX_TAGS);
     // タグを2つに分割
     const mid = Math.ceil(limitedTags.length / 2);
     const firstRowTags = limitedTags.slice(0, mid);
@@ -80,55 +82,84 @@ export const SpotCard = ({
 
     return (
         <ThemeProvider theme={theme}>
-            <div className='spotCard'>
-                {/* ヘッダー部分　タイトル,営業中,クローズボタン */}
-                <div className='cardHeader'>
-                    <div className='title'>{spotName}</div>
+            <div className={styles.spotCard}>
+                <div className={styles.cardHeader}>
+                    <div className={styles.title}>{spotName}</div>
+
                     {typeof isOpen === 'boolean' && (
-                        <div className={`statusBadge ${isOpen ? 'open' : 'closed'}`}>{isOpen ? "営業中" : "営業時間外"}</div>
+                    <div
+                        className={`${styles.statusBadge} ${
+                        isOpen ? styles.open : styles.closed
+                        }`}
+                    >
+                        {isOpen ? "営業中" : "営業時間外"}
+                    </div>
                     )}
-                    <IconButton onClick={onCloseClick} size="small"><X></X></IconButton>
+
+                    <IconButton onClick={onCloseClick} size="small">
+                    <X />
+                    </IconButton>
                 </div>
 
-                {/* 画像部分 */}
-                <div className='cardImage'>
+                <div className={styles.cardImage}>
                     {imageSrc ? (
-                        <img src={imageSrc} alt='画像なし' />
+                    <Image src={imageSrc} alt="画像なし" fill style={{ objectFit: "cover" }}/>
                     ) : (
-                        <div className='noImagePlaceholder'><span>No Image</span></div>
+                    <div className={styles.noImagePlaceholder}>
+                        <span>No Image</span>
+                    </div>
                     )}
                 </div>
 
-                {/* コンテンツ部分　タグ,価格帯 */}
-                <div className='cardContents'>
-                    <div className='row'>
-                        <div className='tagsRow'>
+                <div className={styles.cardContents}>
+                    <div className={styles.row}>
+                        <div className={styles.tagsRow}>
                             {firstRowTags.map((tag, index) => (
-                                <span key={index} className='tag'>#{tag}</span>
+                            <span key={index} className={styles.tag}>
+                                #{tag}
+                            </span>
                             ))}
                         </div>
-                        <div className='pieceRange'>
-                            <span className='icon' style={{ backgroundColor: config.bg1}}>{config.icon1}</span>
+
+                        <div className={styles.pieceRange}>
+                            <span
+                            className={styles.icon}
+                            style={{ backgroundColor: config.bg1 }}
+                            >
+                            {config.icon1}
+                            </span>
                             {price1 || "-"}
                         </div>
                     </div>
-                    <div className='row'>
-                        <div className='tagsRow'>
+
+                    <div className={styles.row}>
+                        <div className={styles.tagsRow}>
                             {secondRowTags.map((tag, index) => (
-                                <span key={index} className='tag'>#{tag}</span>
+                            <span key={index} className={styles.tag}>
+                                #{tag}
+                            </span>
                             ))}
                         </div>
-                        <div className='pieceRange'>
-                            <span className='icon' style={{ backgroundColor: config.bg2}}>{config.icon2}</span>
+
+                        <div className={styles.pieceRange}>
+                            <span
+                            className={styles.icon}
+                            style={{ backgroundColor: config.bg2 }}
+                            >
+                            {config.icon2}
+                            </span>
                             {price2 || "-"}
                         </div>
                     </div>
                 </div>
 
-                {/* フッター部分　ルートボタン,詳細ボタン */}
-                <div className='cardFooter'>
-                    <Button onClick={onDetailClick} variant='contained'>もっと詳しく</Button>
-                    <Button onClick={onRouteClick} variant='outlined'>ルートを見る</Button>
+                <div className={styles.cardFooter}>
+                    <Button onClick={onDetailClick} variant="contained">
+                    もっと詳しく
+                    </Button>
+                    <Button onClick={onRouteClick} variant="outlined">
+                    ルートを見る
+                    </Button>
                 </div>
             </div>
         </ThemeProvider>
