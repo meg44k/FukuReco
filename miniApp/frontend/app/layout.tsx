2d8c9e21c2fa98b64f12b2b607f37ed9b26189d5
meg44k <yugomegane@gmail.com>
2026-02-28 22:34:23 +0900

    Add: すべての画面でMenuBarが画面下に固定されるようにしました

--- miniApp/frontend/app/layout.tsx
+++ miniApp/frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { LIFFProvider } from "../providers/liff-providers";
+import MenuBar from "../components/atoms/menuBar/MenuBar"
+import styles from "./page.module.css";
 
 export const metadata: Metadata = {
   title: "LIFF App",
@@ -23,6 +25,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <LIFFProvider>{children}</LIFFProvider>
+        <div className={styles.menuBar}>
+        <MenuBar/>
+        </div>
       </body>
     </html>
   );
