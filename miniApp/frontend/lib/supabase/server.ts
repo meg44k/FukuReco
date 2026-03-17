e0f5fb2c12b008fc9c0277843cefcf5025f96d8a
ohgushi0329 <k21033ko@apps.kct.ac.jp>
2026-03-17 15:38:57 +0900

    Add:supabaseクライアントの関数にコメントアウトを追加しました

--- miniApp/frontend/lib/supabase/server.ts
+++ miniApp/frontend/lib/supabase/server.ts
@@ -1,6 +1,9 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
+
+// サーバ側(Server Components)でsupabaseに接続するためのクライアントを作成する関数
+// LINEログイン機能を導入した場合などを想定して、Cookieの読み書きの機能を実装してます
 export async function createClient() {
   const cookieStore = await cookies();
 
@@ -9,9 +12,11 @@ export async function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
+        // Cookie読み込み
         getAll() {
           return cookieStore.getAll()
         },
+        // Cookie書き込み
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
