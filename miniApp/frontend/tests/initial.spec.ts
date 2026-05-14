60266006800a029e9a99ecfea6341e9c3cdde726
meg44k <yugomegane@gmail.com>
2026-05-14 10:09:31 +0900

    test: favoritesページにビジュアルレグレッションテストを追加
    PlaywrightのtoHaveScreenshotを使用して、favoritesページの見た目を検証するVRT（Visual Regression Testing）を追加しました。これに伴い、ベースラインとなるスナップショット画像を追加しています。

--- miniApp/frontend/tests/initial.spec.ts
+++ miniApp/frontend/tests/initial.spec.ts
@@ -5,5 +5,17 @@ test('favoritesページにアクセスでき、タイトルが表示される�
 
   const title = page.getByText('保存済み');
   await expect(title).toBeVisible();
-})
+});
 
+
+test('[VRT]favoritesページの見た目が変わっていないか', async ({ page }) => {
+  await page.goto('/favorites');
+
+  await page.getByText('保存済み');
+
+
+  // Screenshotの正解とあっているかどうかを確認する。
+  // 正解画像がなければ撮影される
+  // もし正解画像を更新したければ `npx playwright test --update-snapshots`で更新できる
+  await expect(page).toHaveScreenshot();
+});
